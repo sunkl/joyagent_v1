c35b9d232ea941956ce0dc304b2bf9c72c2c4506
sunkunlun <934926112@qq.com>
2025-10-15 11:42:26 +0800

    fixbug:修复知识库建表问题

--- genie-backend/src/main/resources/db/schema.sql
+++ genie-backend/src/main/resources/db/schema.sql
@@ -69,14 +69,14 @@ CREATE TABLE employee_info (
 CREATE TABLE salary_info (
     salary_id BIGINT PRIMARY KEY  COMMENT '薪酬记录ID',
     employee_id VARCHAR(20) NOT NULL COMMENT '员工ID（用于外键关联employee_info表）',
-    grade int NOT NULL COMMENT '薪酬等级（1-15级）',
-    level int NOT NULL COMMENT '薪档（1-5档）',
+    grade VARCHAR(100) COMMENT '薪酬等级（1-15级）',
+    level VARCHAR(100) COMMENT '薪档（1-5档）',
     base_salary int NOT NULL COMMENT '基本工资'
 ) COMMENT='员工薪酬标准表';
 
 CREATE TABLE employee_attendance (
     attendance_id INT PRIMARY KEY COMMENT '行 ID',
-	employee_id int COMMENT '员工ID',
+	employee_id VARCHAR(20) COMMENT '员工ID',
     clock_in_time VARCHAR(20)  COMMENT '上班打卡时间',
     clock_out_time VARCHAR(20)  COMMENT '下班打卡时间',
     clock_in_type varchar(100) COMMENT '打卡方式（指纹,人脸识别, IC卡, 手机APP, 手动补录)'
